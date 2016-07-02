cef1cb462119258a3e365a84131c52e3cb58bb93
andrey9828 <aaa73731919@gmail.com>
2016-07-02 19:36:16 +0300

    Added task_3

---
+++ task_3/MyArrayTest.php
@@ -0,0 +1,25 @@
+<?php
+
+require('MyArray.php');
+
+class MyArrayTest extends PHPUnit_Framework_TestCase
+{
+  private $my_array;
+
+  protected function setUp()
+  {
+    $this->my_array = new MyArray();
+  }
+
+  protected function tearDown()
+  {
+    $this->my_array = null;
+  }
+
+  public function testQuickSort()
+  {
+    $result = $this->my_array->quickSort(array(8, 3, 7, 2, 1, 5, 2, 7));
+    $expect = array(1, 2, 2, 3, 5, 7, 7, 8);
+    $this->assertEquals($expect, $result);
+  }
+}
