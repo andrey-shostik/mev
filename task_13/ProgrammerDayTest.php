2faff40a2f8d8391109eebe1bc5b945d17a2c671
andrey9828 <aaa73731919@gmail.com>
2016-07-08 03:49:23 +0300

    Added task_13

---
+++ task_13/ProgrammerDayTest.php
@@ -0,0 +1,22 @@
+<?php
+
+require('ProgrammerDay.php');
+
+class ProgrammerDayTest extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->day = new ProgrammerDay();
+    }
+
+    protected function tearDown()
+    {
+        $this->day = null;
+    }
+
+    public function testPrintProgrammerDay()
+    {
+        $result = $this->day->printProgrammerDay($this->day->getYear());
+        $this->assertInternalType("string", $result);
+    }
+}
