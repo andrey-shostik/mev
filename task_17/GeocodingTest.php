145c044778139a857ce4d537563543da0b8f0989
andrey9828 <aaa73731919@gmail.com>
2016-07-11 09:45:34 +0300

    Added task_17

---
+++ task_17/GeocodingTest.php
@@ -0,0 +1,28 @@
+<?php
+
+require('Geocoding.php');
+
+class GeocodingTest extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->obj = new Geocoding;
+    }
+
+    protected function tearDown()
+    {
+        $this->obj = null;
+    }
+
+    public function testGeocode()
+    {
+        $result = $this->obj->geocode('Канев');
+        $this->assertEquals(array(49.751033, 31.4697), $result);
+    }
+
+    public function testReverseGeocode()
+    {
+        $result = $this->obj->reverseGeocode(49.751033, 31.4697);
+        $this->assertInternalType('array', $result);
+    }
+}
